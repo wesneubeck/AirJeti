982f2ba9cde7f0ef3a22e25f85d073c2570422a3
Wes Neubeck <wesneubeck@gmail.com>
2019-02-21 19:34:11 -0500

    D6/restructure readme/added rocks & speed

--- README.md
+++ README.md
@@ -1,53 +1,56 @@
-AirJeti, day = 4, *My 100 day challenge* 
+AirJeti, day = 6, *My 100 day challenge* 
 
-*
-built the bullet and firing functions
-working on: enemy/attacker
-	moves down the screen, firing every 5 seconds
-	random position on the screen 
-	4 attackers each screen
-		one killed, one appears on the top
-		one reaches y = 0, one appears on the top 
-*
+#what I've completed
+	attacker repeat when reachs bottom
+	added rocks/speed
+#what I'm working on
+	rocks repeat at random y value btw 1, 200/ only goes 2 times through/ WHY???
+
+game states:
+	new game = start of game
+		init game = starts when RETURN is pressed 
+	running = while loop
+	game over = when lives = 0
+		screen goes white, says "Game Over" in red
+	level complete = all of enemy killed
 
 
+elements:
+	player
+		green 
+	attacker
+		purple
+	rocks
+		grey
+	bullets	
+		player color
+		attacker color 
 
-1 main class
-background = constant 
+movement:
+	player = east to west
+	enemey = random x.axis, north to south
+	bullet = [[player: position facing, south to north], 
+				[attacker: position facing, north to south]]
 
-attacker()
-	triangle
-	moves left to right on x axis
-		speed
-	facing() = rotate range(160degrees), shooting left to right
-	green
+#create scoreboard
+keep track of:
+	attacker_count = 20
+	score_count = # of attacker killed 
+		if	pygame.sprite.collide_rect(player bullet, attacker): #boolen
+			score_count +=1	
+			attacker_count -=1
 
-enemy()
-	rotating squares	
-	moves up to down on y axis at constant speed
-		speed = random.value(set_range) 
-	rotation, shooting left to right- range(45degrees)
-	purple
+	time = 1 min clock, countdown
 
-rocks()
-	circles(astroid)
-	moves north to south
-		speed = random.value(set_range)
-	no shooting
-	rocks.size = random.value(boundry) #so it can't be too big
-	grey
-
-score() = number of enemy killed
-
-check input from keyboard
-fire() = spacebar
-	bullets = dots(green)
-	**fires in the direction 'player' pointing
-
-reload() = return count
-	wait 3 seconds to fire again
-	killed() = bullets fired hit enemy
-
-movement()
+	health = number of times attacker hits player with bullets or rocks
+		if	pygame.sprite.collide_rect(attacker bullet, player):
+			health -=1
+		if	pygame.sprite.collide_rect(rocks, player):
+			health -=1
+			if health < 1:
+				game state = new game
+		
+	lives = starts with 3, lives-1 when health reaches 0 in game
 	
+
 
--- game.py
+++ game.py
@@ -2,24 +2,48 @@
 
 import sys
 import pygame
+import random
 from pygame.locals import *
-#import random
 
-# window
+# window colors variables
 W = 900
 H = 600
+#screen size
 size = (W, H)
 # player/bullet color - green
+# attacker/bullet color - purple
 green = [0, 250, 0]
-bullet_w = 2
-bullet_h = 12
+purple = [186, 85, 211]
+grey = [134, 136, 138]
 
-"""
+
 class Attacker(pygame.sprite.Sprite):
-	def __init__(self):
-"""		
+	def __init__(self, color):
+		super().__init__()
+		self.image = pygame.Surface([30, 30])
+		self.image.fill(color)
+		self.rect = self.image.get_rect()
+	def update(self):
+		self.rect.y += 4
+		# when attacker reaches the bottom, repeat back to top
+		if self.rect.y == 600:
+			self.rect.y = 0
 
 
+class Rocks(pygame.sprite.Sprite):
+	def __init__(self, color):
+		super().__init__()
+		self.x = random.randint(15, 65)
+		self.y = random.randint(15, 65)
+		self.image = pygame.Surface([self.x, self.y])
+		self.image.fill(color)
+		self.rect = self.image.get_rect()
+	def update(self):
+		self.rect.y += 6
+		# when rock reaches the bottom, repeat back to random spot btw 0, 200
+		if self.rect.y == 600:
+			self.rect.y = random.randint(1, 200)
+				
 
 class Bullet(pygame.sprite.Sprite):
 	 """ This class represents the bullet . """
@@ -29,13 +53,14 @@ class Bullet(pygame.sprite.Sprite):
 		  self.image = pygame.Surface([4, 10])
 		  self.image.fill(green)
 		  self.rect = self.image.get_rect()
-
 	 def update(self):
 		  """ Move the bullet. """
-		  self.rect.y -= 8
+		  self.rect.y -= 20
 
 all_sprites_list = pygame.sprite.Group()
 bullet_list = pygame.sprite.Group()
+attacker_list = pygame.sprite.Group()
+rocks_list = pygame.sprite.Group()
 
 class Main:
 	def __init__(self):
@@ -44,31 +69,46 @@ class Main:
 		self.display = pygame.display.set_mode(size)
 		pygame.display.set_caption('Aviators')
 
-		# background image
+# background image
 		self.bg = pygame.image.load("background.jpg")
 		self.bg = pygame.transform.scale(self.bg, size)
 		self.init_game()
-	
+
 	def init_game(self):
 		self.player_top = [450, 555]
 		self.player_left = [435, 580]
 		self.player_right = [465, 580]
 		self.player = pygame.draw.polygon(self.display, green, (self.player_top, self.player_left, self.player_right), 3)
 
-
+# loop for attackers 
+		for attacker in range(4):
+			attacker = Attacker(purple)
+			attacker.rect.x = random.randint(0,900)
+			all_sprites_list.add(attacker)
+			attacker_list.add(attacker)						
+#			all_sprites_list.update()
+		
+		for rocks in range(8):
+			rocks = Rocks(grey)
+			rocks.rect.x = random.randint(0,900)
+			all_sprites_list.add(rocks)
+			rocks_list.add(rocks)						
+#			all_sprites_list.update()
+		
+	
 	def check_input(self):
 		keys = pygame.key.get_pressed()
 		if keys[pygame.K_f]:
 			#left move	
-			self.player_top = [self.player_top[0]-5, 555]
-			self.player_left = [self.player_left[0]-5, 580]
-			self.player_right = [self.player_right[0]-5, 580]
+			self.player_top = [self.player_top[0]-15, 555]
+			self.player_left = [self.player_left[0]-15, 580]
+			self.player_right = [self.player_right[0]-15, 580]
 		
 		if keys[pygame.K_j]:
 			#right move	
-			self.player_top = [self.player_top[0]+5, 555]
-			self.player_left = [self.player_left[0]+5, 580]
-			self.player_right = [self.player_right[0]+5, 580]
+			self.player_top = [self.player_top[0]+15, 555]
+			self.player_left = [self.player_left[0]+15, 580]
+			self.player_right = [self.player_right[0]+15, 580]
 	
 		if keys[pygame.K_l]:
 			print('working')
@@ -81,26 +121,26 @@ class Main:
 		running = True
 		while running:
 			self.display.blit(self.bg, (0,0))
+			self.clock = pygame.time.Clock()
 			self.check_input()
 
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					running = False
 					sys.exit()
-
 				elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-					self.bullet = Bullet()
-					self.bullet.rect.x = self.player_top[0]
-					self.bullet.rect.y = self.player_top[1]
-					all_sprites_list.add(self.bullet)
-					bullet_list.add(self.bullet)
+					self.player_bullet = Bullet()
+					self.player_bullet.rect.x = self.player_top[0]
+					self.player_bullet.rect.y = self.player_top[1]
+					all_sprites_list.add(self.player_bullet)
+					bullet_list.add(self.player_bullet)
 
 			all_sprites_list.update()
 			all_sprites_list.draw(self.display)
 			pygame.draw.polygon(self.display, green, (self.player_top, self.player_left, self.player_right))
 			pygame.display.flip()
 			pygame.display.update()
-			self.clock.tick(80)
+			self.clock.tick(40)
 
 if __name__ == "__main__":
 	Main().run()
